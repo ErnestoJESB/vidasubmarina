800e0986b68418ceef486cd80a33b069e1c7be1c
ErnestoJESB <jose.jesb25@gmail.com>
2023-06-13 10:34:43 -0500

    Incluíste el Browserrouter v1

--- client/src/components/header/register.jsx
+++ client/src/components/header/register.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import "./login.css"
-
-
-
+import Validation from "./RegisterValidation";
 
 const Register = () => {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        name: '',
+        lastname: '',
+        phone: '',
+        address: '',
+        password2: ''
 
+    });
+    const [errors, setErrors] = useState({});
+
+    const handleInput = (event) => {
+        setValues(prev => ({...prev, [event.target.name]: event.target.value}));
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(Validation(values));
+
+    }
     return (
-        < div className="login-form-container" >
-            <form action="">
+        < div className="login-form-container active" >
+            <form action="" onSubmit={handleInput}>
                 <h3>Registro de usuario</h3>
                 <input type="email" placeholder="example@mwold.net" className="box" />
-                <input type="text" placeholder="nombre" className="box" />
+                <input type="text" placeholder="nombre" className="box" />     
                 <input type="text" placeholder="apellido" className="box" />
                 <input type="text" placeholder="número de teléfono" className="box" />
                 <input type="text" placeholder="dirección" className="box" />
@@ -19,7 +36,7 @@ const Register = () => {
                 <input type="password" placeholder="confirmar contraseña" className="box" />
                 <input type="submit" defaultValue="Iniciar sesión" className="btn" />
                 <p>
-                    ¿Ya tienes una cuenta? <a href="#">Inicia sesión</a>{" "}
+                    ¿Ya tienes una cuenta? <a href="http://localhost:5173/">Inicia sesión</a>{" "}
                 </p>
             </form>
         </div >
