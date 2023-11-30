bac802ee5ff38360b188c84444e7dd8c3aa0ad6c
ErnestoJESB <108556821+ErnestoJESB@users.noreply.github.com>
2023-11-29 23:44:38 -0500

    Perfil

--- client/src/components/empresas/perfilEmpresa.jsx
+++ client/src/components/empresas/perfilEmpresa.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import Empresa from './empresa';
-import Producto from '../producto/producto';
-import Productos from '../cards_principal/carrousel';
+import Perfil from './perfil';
 
 const Perfilempresa = ({ idUser }) => {
+    const userId = idUser;
+
+    console.log(userId);
     const [empresaData, setEmpresa] = useState([]);
     const [productos, setProductos] = useState([]);
     const [idEmp, setIdEmp] = useState([]);
     useEffect(() => {
-        fetch(`http://localhost:3000/empresa/${idUser}`)
+        fetch(`http://localhost:3000/empresa/${userId}`)
             .then(response => response.json())
             .then(data => {
                 setEmpresa(data);
                 setIdEmp(data[0].idempresa);
+                console.log(data[0].idempresa);
+                console.log(data);
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -20,22 +23,7 @@ const Perfilempresa = ({ idUser }) => {
     }, []);
     return (
         <div>
-            <Empresa data={empresaData} />
-            <section className="category">
-                <a href={`http://localhost:5173/registrarproducto/${idEmp}`} className="box">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-plus" width="56" height="56" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" /><path d="M12 9v6" /></svg>
-                    <h3>Registrar producto</h3>
-                </a>
-                <a href={`http://localhost:5173/productosemp/${idEmp}`} className="box">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-alert-circle" width="56" height="56" viewBox="0 0 24 24" strokeWidth="2" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
-                        <path d="M12 8v4" />
-                        <path d="M12 16h.01" />
-                    </svg>
-                    <h3>Ver productos</h3>
-                </a>
-            </section>
+            <Perfil idEmp={idEmp} empresaData={empresaData} />
         </div>
     );
 }
